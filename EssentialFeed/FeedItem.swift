cddab8cefa3e47293e371bbf944c634cae452384
iOSjess <iOSjess@icloud.com>
2024-02-03 16:56:42 -0500

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jessie Elliott on 2/3/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
