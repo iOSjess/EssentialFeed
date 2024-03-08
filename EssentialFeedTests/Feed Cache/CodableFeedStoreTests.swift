f160c32c8d908375ecfce5fb0e9740e51c1ac28e
iOSjess <iOSjess@icloud.com>
2024-03-07 19:20:04 -0500

    Dispatch `CodableFeedStore` operations in a serial background queue to avoid blocking clients

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -179,7 +179,18 @@ class CodableFeedStoreTests: XCTestCase {
             deletionError = receivedDeletionError
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 1.0)
+        /// 
+        /// Updated timeout to 2.0 seconds
+        /// It's an implementation detail of the file system.
+        /// We don't have access to the implementation code but it seems like it uses locks to avoid race conditions mutating the file 
+        /// from different queues/threads. Depending on the queue, OS, and other environment details,
+        /// locks may be applied, slowing down the results.
+        ///
+        /// The other proposed solution was to use `.systemDomainMask` instead of `userDomainMask`
+        /// but that was for running on iOS simulatior
+        ///
+        wait(for: [exp], timeout: 2.0)
+        
         return deletionError
     }
     
