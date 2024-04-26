a14a4bd3266b541049d5ed13065979fa917131cd
iOSjess <iOSjess@icloud.com>
2024-04-25 21:01:23 -0400

    Rename FeedViewControllerTests to FeedUIIntegrationTests since we are testing the composition of multiple UI components in integration

--- EssentialFeediOSTests/Feed UI/Controllers/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/Feed UI/Controllers/FeedUIIntegrationTests.swift
@@ -1,5 +1,5 @@
 //
-//  FeedViewControllerTests.swift
+//  FeedUIIntegrationTests.swift
 //  EssentialFeediOSTests
 //
 //  Created by Jessie Elliott on 3/24/24.
@@ -10,7 +10,7 @@ import UIKit
 import EssentialFeed
 import EssentialFeediOS
 
-final class FeedViewControllerTests: XCTestCase {
+final class FeedUIIntegrationTests: XCTestCase {
     
     func test_feedView_hasTitle() {
         let (sut, _) = makeSUT()
