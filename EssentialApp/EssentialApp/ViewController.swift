a0126f8aa01575ef30a8f2ab3f1fbc55b193e507
iOSjess <iOSjess@icloud.com>
2024-05-12 19:48:42 -0400

    Add empty `EssentialApp` project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Jessie Elliott on 5/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
