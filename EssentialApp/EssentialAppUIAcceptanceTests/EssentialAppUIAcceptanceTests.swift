f987b0fc4fed64694beab65a4613f5743ae651c2
iOSjess <iOSjess@icloud.com>
2024-06-01 17:03:56 -0400

    Displays cached feed on launch when customer has no connectivity

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -8,15 +8,6 @@
 import XCTest
 
 final class EssentialAppUIAcceptanceTests: XCTestCase {
-
-//    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
-//        let app = XCUIApplication()
-//        
-//        app.launch()
-//        
-//        XCTAssertEqual(app.cells.count, 22) <- passes
-//        XCTAssertEqual(app.cells.firstMatch.images.count, 1) <- does not pass
-//    }
     
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
@@ -28,5 +19,20 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         
         let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstImage.exists) // <- does not pass
+    }
+    
+    func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
+        let onlineApp = XCUIApplication()
+        onlineApp.launch()
+        
+        let offlineApp = XCUIApplication()
+        offlineApp.launchArguments = ["-connectivity", "offline"]
+        offlineApp.launch()
+        
+        let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(cachedFeedCells.count, 22) // <- does not pass
+        
+        let cachedFirstImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(cachedFirstImage.exists) // <- does not pass
     }
 }
