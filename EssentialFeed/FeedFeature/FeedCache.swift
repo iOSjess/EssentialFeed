e4893e29fe86ed75274a436e78708c5f0b49a111
iOSjess <iOSjess@icloud.com>
2025-05-25 23:39:06 -0400

    Make FeedCache sync

--- EssentialFeed/FeedFeature/FeedCache.swift
+++ EssentialFeed/FeedFeature/FeedCache.swift
@@ -8,26 +8,5 @@
 import Foundation
 
 public protocol FeedCache {
-    typealias Result = Swift.Result<Void, Error>
-
     func save(_ feed: [FeedImage]) throws
-
-    @available(*, deprecated)
-    func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void)
-}
-
-public extension FeedCache {
-    func save(_ feed: [FeedImage]) throws {
-        let group = DispatchGroup()
-        group.enter()
-        var result: Result!
-        save(feed) {
-            result = $0
-            group.leave()
-        }
-        group.wait()
-        return try result.get()
-    }
-    
-    func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void) {}
 }
